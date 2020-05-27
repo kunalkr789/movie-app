217d37dd6dc58d9c500ac8af63f10fdfdc2fdaee
Kunal Kumar <kunalkr789@gmail.com>
2020-05-27 23:34:03 +0530

    create actions, reducers and subscribe to the store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const store = createStore(movies);
+console.log('store', store);
+ReactDOM.render(<App store={store}/>,  document.getElementById('root'));
 
